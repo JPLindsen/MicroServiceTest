dc31fe212ce589fd08823e09971604401c0d1c29
Job Lindsen <Job.Lindsen@avantiagroup.com>
2017-12-08 19:11:32 +0000

    update

--- simpleservice/src/main/java/com/thoughtmechanix/simpleservice/Application.java
+++ simpleservice/src/main/java/com/thoughtmechanix/simpleservice/Application.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.io.*;
+import hex.genmodel.easy.RowData;
+import hex.genmodel.easy.EasyPredictModelWrapper;
+import hex.genmodel.easy.prediction.*;
+import model_test.GLM_model_R_1511970560428_1;
+
 @SpringBootApplication
 @RestController
 @RequestMapping(value="hello")
@@ -19,7 +25,18 @@ public class Application {
     @RequestMapping(value="/{firstName}/{lastName}",method = RequestMethod.GET)
     public String hello( @PathVariable("firstName") String firstName,
                          @PathVariable("lastName") String lastName) {
+    	
+    	GLM_model_R_1511970560428_1 glmModel = new GLM_model_R_1511970560428_1(); // POJO model
+        EasyPredictModelWrapper model = new EasyPredictModelWrapper(glmModel);
+        
+        RowData row = new RowData();
+         row.put("AGE", 65.0);
+         row.put("RACE", 1.0);
+         row.put("PSA", 1.4);
+         row.put("GLEASON", 6.0);
 
-        return String.format("{\"message\":\"Hello %s %s\"}", firstName, lastName);
+        BinomialModelPrediction p = model.predictBinomial(row);
+
+        return String.format("{\"message\":\"Hello %s %s %4.3f\"}", firstName, lastName, p.classProbabilities[1]);
     }
 }
